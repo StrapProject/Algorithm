0b068f1e62d1c3bad48198194fea7d2674b3777c
LeadDEV <45937776+LeadProject@users.noreply.github.com>
2022-04-06 20:19:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 # HOST FILE
 
-import flask
+from flask import *
+
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+def loginMethod(id, pw):
+    return True
+
+def get_like_contents(id, pw):
+    if loginMethod(id, pw) == True:
+        return ["test"]
+    else:
+        return {"message": "id, pw is not correct.", "error": True}
+
+@app.route('/api/v1/like_contents')
+def like_contents():
+    def get(self, id, pw):
+        return {"contents": get_like_contents(id, pw)}
